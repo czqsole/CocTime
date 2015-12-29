ed31bf96eccdfbc17267353e51086040d5a1ba79
czqsole <czqsole@gmail.com>
2015-12-29 17:52:01 +0800

    2

--- src/coc/Army.java
+++ src/coc/Army.java
@@ -7,11 +7,13 @@ public class Army {
 	public int level;
 	public int numbers;
 	public int size;
+	public int whitchCamp;
 	
 	public Army(){}
 	
 	public Army(String kind, int level) {
 		this.kind = kind;
 		this.level = level;
+		this.whitchCamp = 0;
 	}
 }
--- src/coc/Calculate.java
+++ src/coc/Calculate.java
@@ -6,24 +6,40 @@ import java.util.List;
 public class Calculate {
 	
 	public List<Army> armys = new LinkedList<Army>();
+	public List<Camp> camps = new LinkedList<Camp>();
 	public int numCamp = 4;
 	
 	public Calculate() {}
 	
 	public void calc() {
 		int totalTime = 0;
+		armys.add(new Army());
 		for(Army army : armys) {
 			totalTime += army.time;
 		}
 		int maxBag = totalTime / numCamp;
 		int[][] f = {};
 		f[0][0] = 0;
+		for(int k = 0;k < 3;k++) {
+			Camp camp = camps.get(k);
 		for(int i=0;i<armys.size();i++) {
-			for(int j=0;i < maxBag;j++){
-				f[i][j] = Math.max(f[i][j], 
-						f[i-1][j-armys.get(i).size] + armys.get(i).time);
-				
+			Army army = armys.get(i);
+			if(army.whitchCamp > 0) continue;
+			for(int j=0;i < maxBag;j++) {
+//				f[i][j] = Math.max(f[i][j], 
+//						f[i-1][j-armys.get(i).size] + armys.get(i).time);
+				if((f[i-1][j-army.size] + army.time) > f[i][j]) {
+					f[i][j] = f[i-1][j-army.size] + army.time;
+					if(camp.res.containsKey(army)) {
+						camp.res.put(army, camp.res.get(army) + 1);
+					} else {
+						camp.res.put(army, 1);
+					}
+					army.whitchCamp = k;
+				}			
 			}
 		}
+		}
+		
 	}
 }
